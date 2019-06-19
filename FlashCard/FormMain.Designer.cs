bccdd68cfa2d68e7f285cd643ea28a1852f08ba3
sim511777 <sim51177@gmail.com>
2019-06-19 23:10:41 +0900

    카드 콤보박스 스타일 변경

--- FlashCard/FormMain.Designer.cs
+++ FlashCard/FormMain.Designer.cs
@@ -33,13 +33,18 @@
          this.cbxDeck = new System.Windows.Forms.ComboBox();
          this.panel2 = new System.Windows.Forms.Panel();
          this.Panel13 = new System.Windows.Forms.Panel();
+         this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
          this.btnSearch = new System.Windows.Forms.Button();
          this.chkAutoChange = new System.Windows.Forms.CheckBox();
-         this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+         this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+         this.splitter1 = new System.Windows.Forms.Splitter();
+         this.panel3 = new System.Windows.Forms.Panel();
          this.panel1.SuspendLayout();
          this.panel2.SuspendLayout();
          this.Panel13.SuspendLayout();
          this.tableLayoutPanel1.SuspendLayout();
+         this.tableLayoutPanel2.SuspendLayout();
+         this.panel3.SuspendLayout();
          this.SuspendLayout();
          // 
          // btnPrev
@@ -47,8 +52,9 @@
          this.btnPrev.Dock = System.Windows.Forms.DockStyle.Fill;
          this.btnPrev.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
          this.btnPrev.Location = new System.Drawing.Point(3, 3);
+         this.btnPrev.Margin = new System.Windows.Forms.Padding(0);
          this.btnPrev.Name = "btnPrev";
-         this.btnPrev.Size = new System.Drawing.Size(67, 217);
+         this.btnPrev.Size = new System.Drawing.Size(127, 89);
          this.btnPrev.TabIndex = 1;
          this.btnPrev.Text = "<=";
          this.btnPrev.UseVisualStyleBackColor = true;
@@ -58,9 +64,10 @@
          // 
          this.btnNext.Dock = System.Windows.Forms.DockStyle.Fill;
          this.btnNext.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-         this.btnNext.Location = new System.Drawing.Point(3, 226);
+         this.btnNext.Location = new System.Drawing.Point(136, 3);
+         this.btnNext.Margin = new System.Windows.Forms.Padding(0);
          this.btnNext.Name = "btnNext";
-         this.btnNext.Size = new System.Drawing.Size(67, 218);
+         this.btnNext.Size = new System.Drawing.Size(128, 89);
          this.btnNext.TabIndex = 1;
          this.btnNext.Text = "=>";
          this.btnNext.UseVisualStyleBackColor = true;
@@ -69,41 +76,42 @@
          // browser
          // 
          this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.browser.Location = new System.Drawing.Point(0, 20);
+         this.browser.Location = new System.Drawing.Point(0, 0);
          this.browser.MinimumSize = new System.Drawing.Size(20, 20);
          this.browser.Name = "browser";
-         this.browser.Size = new System.Drawing.Size(842, 478);
+         this.browser.Size = new System.Drawing.Size(658, 505);
          this.browser.TabIndex = 4;
          // 
          // cbxCard
          // 
          this.cbxCard.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.cbxCard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+         this.cbxCard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
          this.cbxCard.FormattingEnabled = true;
-         this.cbxCard.Location = new System.Drawing.Point(201, 0);
+         this.cbxCard.Location = new System.Drawing.Point(0, 49);
          this.cbxCard.Name = "cbxCard";
-         this.cbxCard.Size = new System.Drawing.Size(581, 20);
+         this.cbxCard.Size = new System.Drawing.Size(267, 363);
          this.cbxCard.TabIndex = 6;
          this.cbxCard.SelectionChangeCommitted += new System.EventHandler(this.cbxCard_SelectionChangeCommitted);
          // 
          // panel1
          // 
          this.panel1.Controls.Add(this.cbxCard);
-         this.panel1.Controls.Add(this.btnUndo);
-         this.panel1.Controls.Add(this.btnRedo);
+         this.panel1.Controls.Add(this.tableLayoutPanel1);
          this.panel1.Controls.Add(this.cbxDeck);
-         this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+         this.panel1.Controls.Add(this.Panel13);
+         this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
          this.panel1.Location = new System.Drawing.Point(0, 0);
          this.panel1.Name = "panel1";
-         this.panel1.Size = new System.Drawing.Size(842, 20);
+         this.panel1.Size = new System.Drawing.Size(267, 507);
          this.panel1.TabIndex = 7;
          // 
          // btnUndo
          // 
-         this.btnUndo.Dock = System.Windows.Forms.DockStyle.Right;
-         this.btnUndo.Location = new System.Drawing.Point(782, 0);
+         this.btnUndo.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.btnUndo.Location = new System.Drawing.Point(0, 0);
+         this.btnUndo.Margin = new System.Windows.Forms.Padding(0);
          this.btnUndo.Name = "btnUndo";
-         this.btnUndo.Size = new System.Drawing.Size(30, 20);
+         this.btnUndo.Size = new System.Drawing.Size(47, 29);
          this.btnUndo.TabIndex = 9;
          this.btnUndo.Text = "<";
          this.btnUndo.UseVisualStyleBackColor = true;
@@ -111,10 +119,11 @@
          // 
          // btnRedo
          // 
-         this.btnRedo.Dock = System.Windows.Forms.DockStyle.Right;
-         this.btnRedo.Location = new System.Drawing.Point(812, 0);
+         this.btnRedo.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.btnRedo.Location = new System.Drawing.Point(47, 0);
+         this.btnRedo.Margin = new System.Windows.Forms.Padding(0);
          this.btnRedo.Name = "btnRedo";
-         this.btnRedo.Size = new System.Drawing.Size(30, 20);
+         this.btnRedo.Size = new System.Drawing.Size(47, 29);
          this.btnRedo.TabIndex = 8;
          this.btnRedo.Text = ">";
          this.btnRedo.UseVisualStyleBackColor = true;
@@ -122,42 +131,59 @@
          // 
          // cbxDeck
          // 
-         this.cbxDeck.Dock = System.Windows.Forms.DockStyle.Left;
+         this.cbxDeck.Dock = System.Windows.Forms.DockStyle.Top;
          this.cbxDeck.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
          this.cbxDeck.FormattingEnabled = true;
-         this.cbxDeck.Location = new System.Drawing.Point(0, 0);
+         this.cbxDeck.Location = new System.Drawing.Point(0, 29);
          this.cbxDeck.Name = "cbxDeck";
-         this.cbxDeck.Size = new System.Drawing.Size(201, 20);
+         this.cbxDeck.Size = new System.Drawing.Size(267, 20);
          this.cbxDeck.TabIndex = 7;
          this.cbxDeck.SelectionChangeCommitted += new System.EventHandler(this.CbxDeck_SelectionChangeCommitted);
          // 
          // panel2
          // 
-         this.panel2.Controls.Add(this.browser);
+         this.panel2.Controls.Add(this.panel3);
+         this.panel2.Controls.Add(this.splitter1);
          this.panel2.Controls.Add(this.panel1);
          this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
          this.panel2.Location = new System.Drawing.Point(0, 0);
          this.panel2.Name = "panel2";
-         this.panel2.Size = new System.Drawing.Size(842, 498);
+         this.panel2.Size = new System.Drawing.Size(930, 507);
          this.panel2.TabIndex = 8;
          // 
          // Panel13
          // 
-         this.Panel13.Controls.Add(this.tableLayoutPanel1);
-         this.Panel13.Controls.Add(this.btnSearch);
          this.Panel13.Controls.Add(this.chkAutoChange);
-         this.Panel13.Dock = System.Windows.Forms.DockStyle.Right;
-         this.Panel13.Location = new System.Drawing.Point(842, 0);
+         this.Panel13.Controls.Add(this.btnSearch);
+         this.Panel13.Controls.Add(this.tableLayoutPanel2);
+         this.Panel13.Dock = System.Windows.Forms.DockStyle.Top;
+         this.Panel13.Location = new System.Drawing.Point(0, 0);
          this.Panel13.Name = "Panel13";
-         this.Panel13.Size = new System.Drawing.Size(73, 498);
+         this.Panel13.Size = new System.Drawing.Size(267, 29);
          this.Panel13.TabIndex = 9;
+         // 
+         // tableLayoutPanel1
+         // 
+         this.tableLayoutPanel1.ColumnCount = 2;
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel1.Controls.Add(this.btnNext, 1, 0);
+         this.tableLayoutPanel1.Controls.Add(this.btnPrev, 0, 0);
+         this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+         this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 412);
+         this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+         this.tableLayoutPanel1.RowCount = 1;
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel1.Size = new System.Drawing.Size(267, 95);
+         this.tableLayoutPanel1.TabIndex = 9;
          // 
          // btnSearch
          // 
-         this.btnSearch.Dock = System.Windows.Forms.DockStyle.Top;
-         this.btnSearch.Location = new System.Drawing.Point(0, 28);
+         this.btnSearch.Dock = System.Windows.Forms.DockStyle.Right;
+         this.btnSearch.Location = new System.Drawing.Point(100, 0);
          this.btnSearch.Name = "btnSearch";
-         this.btnSearch.Size = new System.Drawing.Size(73, 23);
+         this.btnSearch.Size = new System.Drawing.Size(73, 29);
          this.btnSearch.TabIndex = 8;
          this.btnSearch.Text = "Search";
          this.btnSearch.UseVisualStyleBackColor = true;
@@ -166,36 +192,53 @@
          // chkAutoChange
          // 
          this.chkAutoChange.AutoSize = true;
-         this.chkAutoChange.Dock = System.Windows.Forms.DockStyle.Top;
+         this.chkAutoChange.Dock = System.Windows.Forms.DockStyle.Fill;
          this.chkAutoChange.Location = new System.Drawing.Point(0, 0);
          this.chkAutoChange.Name = "chkAutoChange";
-         this.chkAutoChange.Size = new System.Drawing.Size(73, 28);
+         this.chkAutoChange.Size = new System.Drawing.Size(100, 29);
          this.chkAutoChange.TabIndex = 6;
-         this.chkAutoChange.Text = "Auto\r\nChange";
+         this.chkAutoChange.Text = "Auto Change";
          this.chkAutoChange.UseVisualStyleBackColor = true;
          // 
-         // tableLayoutPanel1
+         // tableLayoutPanel2
          // 
-         this.tableLayoutPanel1.ColumnCount = 1;
-         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-         this.tableLayoutPanel1.Controls.Add(this.btnNext, 0, 1);
-         this.tableLayoutPanel1.Controls.Add(this.btnPrev, 0, 0);
-         this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 51);
-         this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-         this.tableLayoutPanel1.RowCount = 2;
-         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-         this.tableLayoutPanel1.Size = new System.Drawing.Size(73, 447);
-         this.tableLayoutPanel1.TabIndex = 9;
+         this.tableLayoutPanel2.ColumnCount = 2;
+         this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel2.Controls.Add(this.btnUndo, 0, 0);
+         this.tableLayoutPanel2.Controls.Add(this.btnRedo, 1, 0);
+         this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Right;
+         this.tableLayoutPanel2.Location = new System.Drawing.Point(173, 0);
+         this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+         this.tableLayoutPanel2.RowCount = 1;
+         this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+         this.tableLayoutPanel2.Size = new System.Drawing.Size(94, 29);
+         this.tableLayoutPanel2.TabIndex = 10;
+         // 
+         // splitter1
+         // 
+         this.splitter1.Location = new System.Drawing.Point(267, 0);
+         this.splitter1.Name = "splitter1";
+         this.splitter1.Size = new System.Drawing.Size(3, 507);
+         this.splitter1.TabIndex = 8;
+         this.splitter1.TabStop = false;
+         // 
+         // panel3
+         // 
+         this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.panel3.Controls.Add(this.browser);
+         this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.panel3.Location = new System.Drawing.Point(270, 0);
+         this.panel3.Name = "panel3";
+         this.panel3.Size = new System.Drawing.Size(660, 507);
+         this.panel3.TabIndex = 9;
          // 
          // FormMain
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(915, 498);
+         this.ClientSize = new System.Drawing.Size(930, 507);
          this.Controls.Add(this.panel2);
-         this.Controls.Add(this.Panel13);
          this.Name = "FormMain";
          this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
          this.Text = "Flash Card";
@@ -205,6 +248,8 @@
          this.Panel13.ResumeLayout(false);
          this.Panel13.PerformLayout();
          this.tableLayoutPanel1.ResumeLayout(false);
+         this.tableLayoutPanel2.ResumeLayout(false);
+         this.panel3.ResumeLayout(false);
          this.ResumeLayout(false);
 
       }
@@ -224,6 +269,9 @@
         private System.Windows.Forms.Button btnUndo;
         private System.Windows.Forms.Button btnRedo;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
 
